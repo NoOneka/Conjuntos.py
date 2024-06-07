2c27b7f9135c44ec34c6ccdc9aea0d9134e49bf2
NoOneka <153552938+NoOneka@users.noreply.github.com>
2024-06-07 17:19:20 -0300

    Add files via upload

--- CojuntosUpdate.py
+++ CojuntosUpdate.py
@@ -1,30 +1,21 @@
-conjunto1 = []
-conjunto2 = []
+def menuConj(array1):
+    array1.clear()
 
-#Funções para as operações entre os conjuntos
-def menuConj():
-    array1 = []
-    array2 = []
+    for i in range(4):
 
-    while True:
-        try:
-            for i in range(4):
+       while True:
+            try:
                 elemento = int(input(f"Digite o elemento {i + 1} do array: "))
                 array1.append(elemento)
-                continue
+                break
 
-            for i in range(4):
-                elemento = int(input(f"Digite o elemento {i + 1} do array2: "))
-                array2.append(elemento)
-                continue
-        except:
-            print("Operação não permitida.Tente digitar um número.")
-            continue
-        finally:
-            print(" ")
-        break
+            except ValueError:
+                print("Operação não permitida.Tente digitar um número.")
 
-    return array1, array2
+    return array1
+
+conjunto1 = []
+conjunto2 = []
 
 def interseccção(array1, array2):
     conj1 = set(array1)
@@ -32,27 +23,33 @@ def interseccção(array1, array2):
     a = conj1.intersection(conj2)
     return sorted(list(a))
 
-def main():
 
-    conjunto1, conjunto2 = menuConj()
+def ops():
+    menuConj(conjunto1)
+    menuConj(conjunto2)
 
-    diferença= lambda x, y: sorted(list(set(x) - set(y)))
+    diferença = lambda x, y: sorted(list(set(x) - set(y)))
     resultadoDiferença = diferença(conjunto1, conjunto2)
 
-    uniao = lambda x, y: sorted(list(set(x) | set (y)))
-    resultadoUniao = uniao(conjunto1,conjunto2)
+    uniao = lambda x, y: sorted(list(set(x) | set(y)))
+    resultadoUniao = uniao(conjunto1, conjunto2)
 
     resultadoIntersec = interseccção(conjunto1, conjunto2)
+    return resultadoIntersec, resultadoUniao,resultadoDiferença
 
-    #Finalização do codigo
+def main():
+    resultadoIntersec, resultadoDiferença, resultadoUniao = ops()
     print("Conjuntos preenchidos: ")
     print(conjunto1)
     print(conjunto2)
 
     op = input("Qual operação deseja realizar entre os conjuntos? 1 - Interseccção / 2 - Diferença / 3 - União: ")
-    if op == "1" : print("Resultado é da intersecção entre os conjuntos é: ", resultadoIntersec)
-    elif op == "2" : print("Resultado da diferença entre os conjuntos é: ", resultadoDiferença)
-    elif op == "3" : print("Resultado da união entre os conjuntos é:" , resultadoUniao)
+    if op == "1":
+        print("Resultado é da intersecção entre os conjuntos é: ", resultadoIntersec)
+    elif op == "2":
+        print("Resultado da diferença entre os conjuntos é: ", resultadoDiferença)
+    elif op == "3":
+        print("Resultado da união entre os conjuntos é:", resultadoUniao)
 
     while True:
         try:
@@ -60,7 +57,7 @@ def main():
             if repetir == 2:
                 print("Saindo...")
                 break
-            elif repetir == 1 :
+            elif repetir == 1:
                 print(main())
             else:
                 print("Operação invalida. Tente novamente...")
@@ -68,7 +65,5 @@ def main():
             print("Operação inválida. Tente novamente...")
 
 
-
-if __name__=="__main__":
-    ProgramaMain = main()
-
+if __name__ == "__main__":
+    main()
